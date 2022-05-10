59373d3232ddfead9a9f317b2d1f52b6d6c8035d
dengyuan <dengyuan@dengyuandeMacBook-Pro.local>
2022-05-10 23:32:56 +0800

    梳理定时脚本的逻辑

--- src/main/java/com/studyroom/cms/CmsApplication.java
+++ src/main/java/com/studyroom/cms/CmsApplication.java
@@ -2,8 +2,10 @@ package com.studyroom.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CmsApplication {
 
     public static void main(String[] args) {
