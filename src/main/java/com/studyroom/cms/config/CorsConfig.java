b1adf9b563d451d75fa02552b82b0e270440ad9b
lechankawaii <65642108+lechankawaii@users.noreply.github.com>
2022-05-10 21:02:19 +0800

    change test

--- src/main/java/com/studyroom/cms/config/CorsConfig.java
+++ src/main/java/com/studyroom/cms/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class CorsConfig implements WebMvcConfigurer {
 
+
+    //001
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
